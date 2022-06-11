e94f52cc035340c0f35d1c07e321c1c7dcee6551
HacAtac <jhackworth00@outlook.com>
2022-06-10 23:25:46 -0500

    working with JSON with native json module

--- py_json.py
+++ py_json.py
@@ -1,1 +1,21 @@
 # JSON is commonly used with data APIS. Here how we can parse JSON into a Python dictionary
+
+import json
+
+# Sample JSON
+userJSON = '{"firstName": "John", "lastName": "Doe", "age": 30}'
+
+# Parse JSON to dictionary or (Object)
+user = json.loads(userJSON) # in JS its JSON.parse
+
+print(user)
+print(type(user))
+print(user['firstName'])
+
+# Parse Dictionary to JSON
+car = {'make': 'Ford', 'model': 'Mustang', 'year': 1970}
+carJSON = json.dumps(car)
+
+print(carJSON)
+print(type(carJSON))
+print(car["make"])
